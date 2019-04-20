adcd2538eee046df765e877338a8c11f558cec47
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2019-04-20 04:05:02 -0700

    Update twosum.cpp

--- TwoSum/twosum.cpp
+++ TwoSum/twosum.cpp
@@ -25,3 +25,32 @@ int main()
 } 
 
 ////-----Using Hash Table-------//Time = O(n), Space = O(n)
+#include <bits/stdc++.h>
+using namespace std; 
+
+vector<int> twoSum(vector<int>& nums, int target) 
+    {
+    map<int,int>mymap;
+    int i=0;
+    for(auto itr=nums.begin(); itr != nums.end(); itr++)
+      mymap.insert(pair<int,int>(*itr,i++));
+    
+    for(auto itr=mymap.begin();itr != mymap.end();++itr)
+    {
+      auto index = mymap.find(target - (itr->first));
+      if(itr != index) 
+      {
+          vector<int> ans{(itr->second)+1,(index->second)+1};
+          return ans;
+      }
+    }  
+    }
+    
+int main() 
+{ 
+   vector<int>vect{2, 5, 11, 8, 7},ans;
+   int target = 9;
+   //Function Call
+   ans = twoSum(vect,target); 
+   cout<<"("<<ans[0]<<" "<<ans[1]<<")";
+} 
